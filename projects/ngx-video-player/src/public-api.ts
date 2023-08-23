8c78a033c74ff222cdf0b50968738909bf0554de
benoit <benoit.tassin@live.fr>
2023-08-23 09:58:16 +0200

    split styles and html in separate files

--- projects/ngx-video-player/src/public-api.ts
+++ projects/ngx-video-player/src/public-api.ts
@@ -2,6 +2,5 @@
  * Public API Surface of ngx-video-player
  */
 
-export * from './lib/ngx-video-player.service';
 export * from './lib/ngx-video-player.component';
 export * from './lib/ngx-video-player.module';
