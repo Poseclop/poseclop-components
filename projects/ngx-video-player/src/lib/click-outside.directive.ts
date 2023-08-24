3caa549bb8a8bf69e729555a445373dc2a991379
benoit <benoit.tassin@live.fr>
2023-08-24 10:20:29 +0200

    clean unused code

--- projects/ngx-video-player/src/lib/click-outside.directive.ts
+++ projects/ngx-video-player/src/lib/click-outside.directive.ts
@@ -1,23 +1,16 @@
-import { Directive, ElementRef, EventEmitter, HostListener, Input, Output } from '@angular/core';
+import { Directive, ElementRef, EventEmitter, HostListener, Output } from '@angular/core';
 
 @Directive({
   selector: '[clickOutside]'
 })
 export class ClickOutsideDirective {
-  @Input() exceptions: ElementRef<HTMLElement>[] = [];
   @Output() clickOutside = new EventEmitter<void>();
 
   constructor(private elementRef: ElementRef<HTMLElement>) { }
 
   @HostListener('document:click', ['$event.target'])
   public onClick(target: Node | null): void {
-    let clickedInside = false;
-    clickedInside = this.elementRef.nativeElement.contains(target);
-
-    if (!clickedInside && this.exceptions.length > 0) {
-      clickedInside = this.exceptions.some(e => e.nativeElement.contains(target));
-    }
-
+    const clickedInside = this.elementRef.nativeElement.contains(target);
     if (!clickedInside) {
       this.clickOutside.emit();
     }
